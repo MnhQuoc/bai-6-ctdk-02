29929a7c0afc20f6756a5f63bb78406e547b5f21
Quốc Nguyễn <nguyenminhquoc812005@gmail.com>
2024-07-17 22:26:05 +0700

    nop bai tap

--- bai-6-02.js
+++ bai-6-02.js
@@ -3,8 +3,8 @@ function giaipt() {
     let b = +document.getElementById("b").value
     var x;
     document.getElementById("pt").innerHTML =  "Giải phương trình: " + a + "x +" + b + " = 0";
-    if (a<=0) {
-        document.getElementById("result").innerHTML =  "a không thể nhỏ hơn hoặc bằng 0 ";
+    if (a === 0) {
+        document.getElementById("result").innerHTML =  "a không thể bằng 0 ";
     }
     else {
         x = -b/a
